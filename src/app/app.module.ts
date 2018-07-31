92a272737c2c75fd767edf575e4f427119b28f87
Julien Chil Hagopian <julienchilhagopian@gmail.com>
2018-07-31 20:44:52 +0200

    code along finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
+// ---- MODULES ---- //
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+// ---- PAGES ---- //
 import { AppComponent } from './app.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+
+// ---- SERVICES ---- //
+import { AuthService } from './services/auth.service';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+
+// ---- GUARDS ---- //
+import { RequireAnonGuardService } from './guards/require-anon-guard.service';
+import { RequireUserGuard } from './guards/require-user.guard';
+
+const routes: Routes = [
+  { path: 'signup', component: SignupPageComponent, canActivate: [RequireAnonGuardService] },
+  { path: 'login', component: LoginPageComponent, canActivate: [RequireAnonGuardService] },
+  { path: 'profile', component: ProfileComponent, canActivate: [RequireUserGuard] }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupPageComponent,
+    ProfileComponent,
+    LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    RequireAnonGuardService,
+    RequireUserGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
